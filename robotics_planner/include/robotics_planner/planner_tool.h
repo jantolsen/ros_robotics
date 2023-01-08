19b041828b06c0655401c9bf497c7ec9869f1a4e
jantolsen <janthomasolsen@gmail.com>
2023-01-08 17:16:21 +0100

    Added planner tools

---
+++ robotics_planner/include/robotics_planner/planner_tool.h
@@ -0,0 +1,47 @@
+// Planner Toolbox 
+// -------------------------------
+// Description:
+//      Toolbox for Planner
+//      Contains several helper and utility functions
+//
+// Version:
+//  0.1 - Initial Version
+//        [05.01.2023]  -   Jan T. Olsen
+//
+// -------------------------------
+
+// Include guard:
+// -------------------------------
+// Prevents double declaration of identifiers (e.g. types, enums, static variables)
+//  #ifndef: 
+//      Check whether header-file with the unique value "xxx_H" is already included
+//  #define: 
+//      If header-file not earlier included, it continues and defines the rest of the file 
+//  #endif: 
+//      End of include guard
+#ifndef PLANNER_TOOL_H       
+#define PLANNER_TOOL_H
+
+// Include Header-files:
+// -------------------------------
+    // Standard
+    #include <memory>
+    #include <iostream>
+    #include <string>
+    #include <vector>
+    #include <map>
+
+    // Ros
+    #include <ros/ros.h>
+
+    // Robotics Toolbox
+    #include <robotics_toolbox/toolbox.h>
+
+// Namespace: Planner Toolbox
+// -------------------------------
+namespace PlannerTool
+{
+    
+
+} // End Namespace: Planner Toolbox
+#endif // PLANNER_TOOL_H 
---
+++ robotics_planner/src/planner_tool.cpp
@@ -0,0 +1,25 @@
+// Planner Toolbox 
+// -------------------------------
+// Description:
+//      Toolbox for Planner
+//      Contains several helper and utility functions
+//
+// Version:
+//  0.1 - Initial Version
+//        [05.01.2023]  -   Jan T. Olsen
+//
+// -------------------------------
+
+// Include Header-files:
+// -------------------------------
+    
+    // Main Header-File
+    #include "robotics_planner/planner_tool.h"
+
+/// Namespace: Planner Toolbox
+// -------------------------------
+namespace PlannerTool
+{
+    
+
+} // End Namespace: Planner Toolbox
